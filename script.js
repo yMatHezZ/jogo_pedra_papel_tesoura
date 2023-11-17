67225735dbdb7084fc5b4804d5cd5a73afc35f39
yMatHezZ <150362833+yMatHezZ@users.noreply.github.com>
2023-11-17 08:43:25 -0300

    terceiro js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const playerOptionsList = document.querySelectorAll(".jogador .option");
+const enemyOptionsList = document.querySelectorAll(".enemy .option");
 
-playerOptionsList.forEach((opcao)=>{
-    opcao.onclick = ()=> {
-        Opcao.style.opacity = "1"
-        opcao.setAttribute("data-selectd", true)
-    }
-}
-
-
-)
+playerOptionsList.forEach((option) => {
+    option.onclick = () => {
+      clearOptions(playerOptionsList);
+  
+      option.style.opacity = "1";
+      option.setAttribute("data-selected", true);
+  
+      iaEnemy();
+  
+      const movePlayer = option.getAttribute("data-value");
+      const moveEnemy = iaEnemy();
+      result(movePlayer, moveEnemy);
+  };
+});
